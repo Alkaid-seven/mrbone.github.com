0c0e1a0feae149375ffd907c1fc3451fb1e12995
Mr-Bone <darknessxu@qq.com>
2013-05-06 11:33:27 +0800

    m

--- js/js/ie6.js
+++
@@ -1,4 +0,0 @@
-$.fn.carousel = function(num){
-	this.find('.carousel-inner > .item').eq(num).addClass('active').show().siblings().removeClass('active').hide();
-	this.find('.carousel-indicators > li').eq(num).addClass('active').siblings().removeClass('active');
-}
